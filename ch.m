771e9b95d15dc389caaaa8d05bd333994cc06b05
Alex Myczko <alex@aiei.ch>
2021-05-09 14:11:32 +0200

    Update ch.m

--- ch.m
+++ ch.m
@@ -6,6 +6,7 @@
    macos: clang -framework Foundation -framework AppKit ch.m -o ch
    linux: gcc -I/usr/include/GNUstep -fconstant-string-class=NSConstantString ch.m -lgnustep-gui -lgnustep-base -lobjc -o ch
    linux: clang -I/usr/lib/gcc/x86_64-linux-gnu/{9,10}/include -I/usr/include/GNUstep ch.m -lgnustep-gui -lgnustep-base -lobjc -o ch
+   gnustep, fix the size: defaults write NSGlobalDomain GSScaleFactor 1.5
 
    Released under the GNU GPL-2-or-later
 */
