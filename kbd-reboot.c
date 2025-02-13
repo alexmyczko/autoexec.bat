786d478a4cbd894b79ca8005ebca907aaae9f113
Alex Myczko <alex@aiei.ch>
2025-02-13 19:16:05 +0100

    Update kbd-reboot.c

--- kbd-reboot.c
+++ kbd-reboot.c
@@ -1,6 +1,8 @@
 /*
 https://www.basicinputoutput.com/2024/11/the-keyboard-controller-interface.html
 apt install ioport; outb 0x64 0xFE
+
+rmmos usbhid # disable kbd input
 */
 
 #include <unistd.h>
