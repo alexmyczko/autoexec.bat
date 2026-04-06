75487f33f9a6759a76de137b31ea19cf4463542b
Alex Myczko <alex@aiei.ch>
2026-04-06 09:18:33 +0000

    Add reference comment for improved pi calculation
    Added a comment with a reference to a better formula for computing pi.

--- pi.sh
+++ pi.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# 20:04 < io> tarzeau: there's a better formula btw: 
+# https://www.johndcook.com/blog/2019/10/29/computing-pi-with-bc/ time bc -l <<< 
+# "scale=10000;16*a(1/5) - 4*a(1/239)"
 if [ "${1}x" = x ]; then
     echo syntax: $0 numberofdigits
     exit 1
